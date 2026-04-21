164373aa617875fcd1d4c4533855de254a7f0a1a
Md Rifat Nahid <108307771+mdrifatnahid@users.noreply.github.com>
2026-04-21 23:51:49 +0200

    Add files via upload

---
+++ Program2.py
@@ -0,0 +1,9 @@
+name = "Lala Datt"
+age = 30
+cgpa = 3.96
+
+print ("Our new student is "+name + "." )
+print (name+"lives in Mumbai.")
+print("He is ", age, "years old.")
+print("At the age of ", age, ", he started learning Python.")
+print (name+ "has scored", cgpa, "in his exam.")

7cbb6e2d1671616e5e5d0b1ece0fb9932688435d
Md Rifat Nahid <108307771+mdrifatnahid@users.noreply.github.com>
2026-04-22 09:52:03 +0200

    Add files via upload

---
+++ Program_5.py
@@ -0,0 +1,19 @@
+
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+result_1 = num1 + num2
+print("The sum of the result is : ",result_1)
+
+result_2 = num1 - num2
+print("The substraction of the result is : ",result_2)
+
+result_3 = num1 / num2
+print("The division of the result is : ",result_3)
+
+result_4 = num1 * num2
+print("The multiplication of the result is : ",result_4)
+
+result_5 = num1 % num2
+print("The remainder of the result is : ",result_5)
